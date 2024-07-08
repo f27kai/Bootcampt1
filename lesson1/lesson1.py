023bfe307f8b52734e881228845ac840cf19cd17
f27kai <feruzakairatbekova@gmail.com>
2024-07-08 20:29:13 +0600

    1

---
+++ lesson1/lesson1.py
@@ -0,0 +1,29 @@
+"""
+    ТЕМА: Питон тилине киришүү жана математикалык амалдар
+"""
+# print("Salam")
+
+"""Типы данных"""
+# str - "Asan", "5", "5.2", "True"
+# int - 1, 0, 56, 7, 5
+# float - 1.2, 3.2, 10.5
+# Bool - True, False
+#
+# name = input("Senin atyn kim? ")
+# print("Senin atyn", name)
+
+# """Математикалык амалдар"""
+# print(5 + 5)
+# print(10 - 5)
+# print(2 * 5)
+# print(2 ** 5)
+# print(15 / 5)
+# print(15 // 5)
+# print(16 % 5)
+
+year = int(input("Senin jylyn kancha? "))
+age = 2024 - year
+age_12 = age + 12
+
+print("Senin azyrky uchurdagy jashyn:", age)
+print("12 jyldan kiyinki jashyn:", age_12)
---
+++ lesson1/lesson3.py
@@ -0,0 +1,107 @@
+"""
+    ТЕМА: цикл while, for
+"""
+
+# i = 5
+#
+# while i <= 25:
+#     i *= 5
+#     print(i)
+
+# while True:
+#     print("Hello")
+
+
+# while True:
+#     x = int(input("x: "))
+#     option = input("+, -, /, *:    ")
+#     y = int(input("y: "))
+#
+#     if option == "+":
+#         print(x + y)
+#     elif option == "-":
+#         print(x - y)
+#     elif option == "/":
+#         if y == 0:
+#             print("Infinity")
+#         else:
+#             print(x / y)
+#     else:
+#         print(x * y)
+#     print()
+
+
+# i = 1
+#
+# while i <= 10:
+#     if i == 5:
+#         print("Finish")
+#         break
+#     print(i)
+#     i += 1
+
+# i = 1
+#
+# while i <= 10:
+#     if i == 5:
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1
+
+
+# while True:
+#     point = input("Enter your points: ")
+#
+#     if point == "exit":
+#         print("Finished ...")
+#         break
+#
+#     point = int(point)
+#
+#     if 500 <= point:
+#         print("Золотой сертификат")
+#     elif 360 <= point <= 499:
+#         print("Серебро")
+#     elif 280 <= point <= 359:
+#         print("Бронза")
+#     elif 100 <= point <= 279:
+#         print("Простой")
+#     else:
+#         print("Тилекке каршы сизге сертификат берилбейт")
+#     print()
+
+
+"""for"""
+
+# world = "Hello World!"
+#
+# for i in world:
+#     print(i)
+
+#
+# for i in range(1, 61):
+#     print(i, "Asan")
+
+# for i in range(101):
+#     print(i)
+
+# for i in range(2, 101, 5):
+#     print(i)
+
+
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i} * {j} = {i * j}")
+#     print()
+
+
+i = 1
+j = 1
+
+while i < 11:
+    while j < 11:
+        
+
+
+
